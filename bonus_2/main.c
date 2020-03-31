585ec77f2e09b9ca73070a00d7bbd87bf061cbe2
Jakub-Bartnicki <jakub_bartnicki@tlen.pl>
2020-03-31 23:49:38 +0200

    Cleaned bonus_2 dir and made some bug fixes

--- bonus_2/main.c
+++ bonus_2/main.c
@@ -3,36 +3,41 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <assert.h>
+
 int licznik=0;
-void* napis (void *arguments)
-{
+
+void* napis (void *arguments) {
   	int index = *((int *)arguments);
 	int k = rand () % 10001;
 	int j;
-	for (j=0; j<k; j++)
-	{
+	for (j=0; j<k; j++) {
 		printf ("Wątek %d iteracja %d\n", index, j);
 		licznik++;
 	}
+	
+	return NULL;
 }
-int main (void)
-{
+
+int main (void) {
 	srand((unsigned int)time(NULL));
 	pthread_t watki[10];
+
 	int watki_args[10];
 	int i;
 	int result_code;
-	for (i=0; i<10; i++)
-	{
+
+	for (i=0; i<10; i++) {
 		watki_args[i] = i;
 		result_code = pthread_create (&watki[i], NULL, napis, &watki_args[i]);
 		assert(!result_code);
 	}
-	for (i=0; i<10; i++)
-	{
+
+	for (i=0; i<10; i++) {
 		result_code = pthread_join (watki[i], NULL);
 		assert (!result_code);
 	}
+
 	printf ("Liczba wszystkich iteracji wywolanej przez wszysktie wątki: %d\n", licznik);
+
 	return 0;
 }
