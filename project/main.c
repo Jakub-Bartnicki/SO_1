623d262fe8615a1812d18a9f2388d812b350841c
Jakub-Bartnicki <jakub_bartnicki@tlen.pl>
2020-04-28 18:10:08 +0200

    added working help and sum

--- project/main.c
+++ project/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
+
 int checkLastSymbol(char array[]) {
     char c = '&';
 
@@ -21,17 +22,33 @@ int main(int argc, const char* argv[]) {
     char option[200];
     int currChar = 0;
 
-    // if (argc == 1) {
-    //     printf("Please provide the command line arguments\n");
-    // }
+    if (argc > 1) {
+        printf("Unnecessary arguments have been ignored\n");
+    }
 
     while (1) {
         char readed[200];
+        char x;
 
-        fgets(readed, 200, stdin);
+        // TODO: Poprawić, cos nie tak z getc
+        // if (x = getc(stdin) == '\033' || x == '\033') { 
+        // getc
+        //     switch(x) {
+        //         case 'A':
+        //             system("history-search-backward");
+        //             break;
+        //         case 'B':
+        //             system("hitory-search-forward");
+        //             break;
+        //     } 
+        // } else { 
+
+        gets(readed);
 
         strcpy(option, readed);
-        strtok(option, " ");
+        if (strcmp(option, "history") != 0 && strcmp(option, "help") != 0 && strcmp(option, "exit") != 0) {
+            strtok(option, " ");
+        }
 
         if (!(strcmp(option, "exit"))) {
             printf("%s", "exitting...\n");
@@ -43,17 +60,28 @@ int main(int argc, const char* argv[]) {
             strcat(command, readed);
 
             printf("command: %s\n", command);
+
             system(command);
+        } else if (!(strcmp(option, ""))) {
+
+        } else if (!(strcmp(option, ""))) {
+            // TODO: Klikanie strzałki w gore daje poprzednia komende
+
+        } else if (!(strcmp(option, ""))) {
+            
+
         } else if (!(strcmp(option, "history"))) {
+            // TODO: Wyswietlanie historii
 
         } else if (!(strcmp(option, "help"))) {
-
+            puts("\nhelp - show this help list\n"                       \
+            "sum [x y z ...] - sum 2 (or more) numbers\n"               \
+            "history - show history of commands\n"                      \
+            "exit - exit this interpreter\n");        
         } else {
             printf("%s", "Choose another option");
         }
 
-        getchar();
     }
-
     return 0;
 }
